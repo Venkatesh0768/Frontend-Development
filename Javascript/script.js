1ca02bbb27633e2d33d1fd4c5ea0092c2b63d4b8
Venkatesh Rapolu <124189195+Venkatesh0768@users.noreply.github.com>
2025-07-25 13:07:39 +0530

    complete the javascript basci to advance and also some question javascript and some on the dom

---
+++ Javascript/script.js
@@ -0,0 +1,115 @@
+// Task 1
+// var btn = document.querySelector("button")
+// var p = document.querySelector("p")
+
+// btn.addEventListener('click' , function(){
+//     p.textContent = "Change Hogaya"
+// })
+
+// Task 2
+
+// var img1 = document.querySelector(".image1");
+// var img2 = document.querySelector(".image2");
+// var btn = document.querySelector("button");
+
+// btn.addEventListener("click", function () {
+//   var src1 = img1.src;
+//   var src2 = img2.src;
+//   img1.src = src2;
+//   img2.src = src1;
+
+// });
+
+
+
+
+// Task 3
+
+// var inps = document.querySelectorAll('input[type="text"]');
+// var form = document.querySelector('form');
+// var h1 = document.querySelector('h1');
+
+// form.addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     for (var i = 0; i < inps.length; i++) {
+//         if (inps[i].value.trim() === '') {
+//             h1.textContent = "This error";
+//             h1.style.color = 'red';
+//             break;
+//         }
+//     }
+// });
+
+
+
+// Task 4 
+
+
+// var h1 = document.querySelector('h1');
+// var start = document.querySelector('#start')
+// var end = document.querySelector('#end')
+
+// var int;
+// start.addEventListener('click' , function(){
+//     var count =0;
+//     int  = setInterval(function(){
+//         h1.textContent = count;
+//         count++;
+//     } , 1000);
+// })
+
+// end.addEventListener('click' , function(){
+//     clearInterval(int)
+// })
+
+
+// Task 5
+
+// var input = document.querySelector('input');
+// var ul = document.querySelector('ul')
+// var add = document.querySelector('#add');
+// var remove = document.querySelector('#remove');
+
+// var li ;
+// add.addEventListener('click' , function(){
+//     if(input.value === ''){}
+//     else{
+//         li =  document.createElement('li')
+//         li.textContent = input.value;
+//         ul.appendChild(li)
+//         input.value ='';
+//     }
+// })
+
+// remove.addEventListener('click', function() {
+//     if (ul.lastChild) {
+//         ul.removeChild(ul.lastChild);
+//     }
+// });
+
+
+// Task 6
+
+// var button = document.querySelectorAll('.nav-btn')
+// var texts = document.querySelectorAll('h3')
+
+// texts[0].style.display = 'block'
+// texts[0].style.width = '50%'
+
+// button.forEach(function(div ,index){
+//     div.addEventListener('click' , function(){
+//         hideAllText();
+//         texts[index].style.display ='block';
+//         texts[index].style.width = '50%';
+//     })
+// })
+
+// function hideAllText(){
+//     texts.forEach(function(text){
+//         text.style.display ='none'
+//     })
+// }
+
+
+
+//Task 7
